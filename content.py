9628fbb6458cda84d98c30aa2553d5092be34a46
VardanMittal <vardanmittal000@gmail.com>
2023-09-08 22:57:14 +0530

    "First Commit"

---
+++ content.py
@@ -0,0 +1,60 @@
+def content(cat,by,to,college):
+    content_text = f"""
+Dear {to},
+
+I hope this message finds you well. My name is Vardan Mittal, and I am an undergraduate student at the National Institute of Technology (NIT), Surat, India. I am writing to express my keen interest in the possibility of joining your esteemed research group at ETH Zurich for a research internship in the field of Machine Learning and Deep Learning, commencing in January 2024 and extending for a duration of 4 to 6 months.
+
+My journey into the world of machine learning and deep learning has been one filled with excitement and a fervent desire to contribute to innovative research endeavors. I have always been deeply passionate about the potential of these technologies to address complex real-world challenges. Over the years, I have honed my skills and knowledge, particularly in the domains of robotics, mechatronics, and computer vision.
+
+One of the most significant projects I have undertaken is in the realm of robotics and mechatronics. I had the privilege of designing and implementing an autonomously navigating robot, leveraging Visual Simultaneous Localization and Mapping (VSLAM) algorithms and computer vision techniques. This project was a testament to my commitment to applying machine learning concepts in practical applications and demonstrated my ability to integrate cutting-edge technology into physical systems.
+
+In addition to my robotics work, I have also gained substantial experience in data analysis and automation. Proficient in programming languages such as Python, C, C++, JavaScript, SQL, and MATLAB, I have utilized these tools to analyze data and create automation scripts. My familiarity with Selenium Web Drivers has enabled me to engage in web scraping and various automation tasks, further enhancing my skill set.
+
+I have attached my resume and cover letter to provide you with a comprehensive overview of my qualifications and experiences, which I believe align well with the research activities at ETH Zurich. While my academic pursuits have focused on electronics and communication engineering, my true passion lies in the practical applications of machine learning and deep learning.
+
+I respectfully request your consideration for an internship opportunity within your research group. I believe that the experience I would gain at ETH Zurich under your mentorship would be invaluable in my academic and research journey. Your guidance would provide me with the opportunity to contribute meaningfully to ongoing projects while furthering my understanding of the intricate world of machine learning and deep learning.
+
+Thank you for your time and consideration.
+
+Warm regards,
+
+{by}
+"""
+    content_html = f"""\
+        <html>
+            <body>
+                <p align=justify>
+                    Dear <strong>{to}</strong>,<br>
+<br>
+I hope this message finds you well. My name is Vardan Mittal, and I am an undergraduate student at the <strong>National Institute of Technology (NIT), Surat, India</strong>. I am writing to express my keen interest in the possibility of joining your esteemed research group at ETH Zurich for a research internship in the field of <strong>Machine Learning and Deep Learning</strong>, commencing in January 2024 and extending for a duration of 4 to 6 months.<br>
+<br>
+
+My journey into the world of machine learning and deep learning has been one filled with excitement and a fervent desire to contribute to innovative research endeavors. I have always been deeply passionate about the potential of these technologies to address complex real-world challenges. Over the years, I have honed my skills and knowledge, particularly in the domains of robotics, mechatronics, and computer vision.<br>
+<br>
+
+One of the most significant projects I have undertaken is in the realm of robotics and mechatronics. I had the privilege of designing and implementing an autonomously navigating robot, leveraging <strong>Visual Simultaneous Localization and Mapping (VSLAM) algorithms</strong> and <strong>computer vision</strong> techniques. This project was a testament to my commitment to applying machine learning concepts in practical applications and demonstrated my ability to integrate cutting-edge technology into physical systems.<br>
+<br>
+
+In addition to my robotics work, I have also gained substantial experience in data analysis and automation. Proficient in programming languages such as<strong> Python, C, C++, JavaScript, SQL, and MATLAB</strong>, I have utilized these tools to analyze data and create automation scripts. My familiarity with Selenium Web Drivers has enabled me to engage in web scraping and various automation tasks, further enhancing my skill set.<br>
+<br>
+
+I have attached my resume and cover letter to provide you with a comprehensive overview of my qualifications and experiences, which I believe align well with the research activities at ETH Zurich. While my academic pursuits have focused on electronics and communication engineering, my true passion lies in the practical applications of machine learning and deep learning.<br>
+<br>
+
+I respectfully request your consideration for an internship opportunity within your research group. I believe that the experience I would gain at {college} under your mentorship would be invaluable in my academic and research journey. Your guidance would provide me with the opportunity to contribute meaningfully to ongoing projects while furthering my understanding of the intricate world of machine learning and deep learning.<br>
+<br>
+
+Thank you for your time and consideration.<br>
+<br>
+
+Warm regards,<br>
+{by}
+                </p>
+            </body>
+        </html>
+    """
+    
+    if(cat == "Text"):
+        return content_text
+    else:
+        return content_html
---
+++ mail.py
@@ -0,0 +1,45 @@
+import smtplib, ssl
+import os
+from dotenv import load_dotenv
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from content import content
+from pathlib import Path
+load_dotenv()
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Variables >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+sender_email = os.environ["SENDER"]
+Theme = "Machine Learning and Automation"
+password = os.environ["PASSWORD"]
+receiver_email = "nskknk17@gmail.com"
+message = MIMEMultipart("alternative")
+message["Subject"] = f"Application for Internship Opportunity in {Theme}(January - May 2024)"
+message["From"] = "Vardan Mittal"
+message["To"] = receiver_email
+FILEPATH = "./docs/"
+part1 = MIMEText(content("text","Vardan Mittal", "Tal"), "plain")
+part2 = MIMEText(content("html","Vardan Mittal", "Tal"), "html")
+
+message.attach(part1)
+message.attach(part2)
+
+############################## Adding Attatchments ##############################
+
+files = [f"{FILEPATH}resume.pdf", f"{FILEPATH}cover_letter.pdf"]
+
+for path in files:
+    part = MIMEBase("application", "octet-stream")
+    with open(path,'rb') as file:
+        part.set_payload(file.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition',
+                        'attachment; filename={}'.format(Path(path).name))
+    message.attach(part)
+##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Send Mail >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>##
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    print("Sending mail....")
+    server.login(sender_email, password)
+    server.sendmail(sender_email,receiver_email,message.as_string())
+print("Mail Sent!!🎉")
