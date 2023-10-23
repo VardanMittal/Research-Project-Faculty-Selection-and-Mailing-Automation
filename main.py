ff7c51d2d1dbb0fec6e93d4b3396bd6a0ac03f01
Vardan Mittal <vardanmittal000@gmail.com>
2023-10-24 01:06:37 +0530

    Main Function Writeup

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from mail import mailSender
 from trigger import trigger
 from data_fetch import data
 
+ZONE = "CET"
+
+if __name__ == "__main__":
+    time = trigger(ZONE)
+    indian = time[1]
+    foreign = time[0]
--- trigger.py
+++ trigger.py
@@ -1,9 +1,16 @@
 from datetime import datetime
 import pytz
 
+
+def struct(time):
+    return {
+        "date":time.date(),
+        "hr":time.time().hour,
+        "min":time.time().minute
+    }
 def trigger(zone):
     indian_timezone = pytz.timezone('Asia/Kolkata')
     indian_time = datetime.now(indian_timezone)
     foreign_timezone = pytz.timezone(zone)
     foreign_time = indian_time.astimezone(foreign_timezone)
-    return foreign_time.time().hour
+    return [struct(foreign_time), struct(indian_time)]
